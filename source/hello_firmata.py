d772ac435be7aed51c7834c53364ba5ea3b8b88c
kramoth <kramoth@udm.ac.mu>
2020-08-05 10:30:26 +0400

    [master] adding arduino script that blinks the arduino LED

--- source/hello_firmata.py
+++ source/hello_firmata.py
@@ -1,12 +1,39 @@
+import sys
 from pyfirmata import Arduino, util
 import time
-board = Arduino('/dev/ttyUSB0')
-board.digital[13].write(1)
-it = util.Iterator(board)
-it.start()
-time.sleep(2)
-while True:
-    board.digital[13].write(1)
-    time.sleep(0.2)
-    board.digital[13].write(0)
-    time.sleep(0.2)
+
+
+
+
+class control_arduino:
+	def __init__(self,port):
+		print(port)
+		self.board=Arduino(port)
+		self.it=util.Iterator(self.board)
+		self.it.start()
+		time.sleep(2)
+		print("ok")
+
+	def run(self):
+		while True:
+			self.board.digital[13].write(1)
+			time.sleep(2)
+			self.board.digital[13].write(0)
+			time.sleep(2)
+
+
+
+if __name__ == '__main__':
+	if len(sys.argv)>1:
+		try:
+			ard=control_arduino(sys.argv[1])
+			ard.run()
+		except Exception as e:
+			print(e)
+			sys.exit(1)
+	else:
+		try:
+			control_arduino("/dev/ttyACM0")
+		except Exception as e:
+			print(e)
+			sys.exit(1)
